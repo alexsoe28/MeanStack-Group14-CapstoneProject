05bb7166c433cb9d1924d80b43ad4d7f6d6ec9d2
Austin C <austin.chau@tcs.com>
2021-04-26 16:30:00 -0700

    added prototype components for admin signin, products, requests, and the signed in landing page

--- FrontEnd/Online-Groceries-frontend/src/app/app.module.ts
+++ FrontEnd/Online-Groceries-frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './components/admin/signin/signin.component';
+import { AddComponent } from './components/admin/products/add/add.component';
+import { DeleteComponent } from './components/admin/products/delete/delete.component';
+import { UpdateComponent } from './components/admin/products/update/update.component';
+import { ViewComponent } from './components/admin/requests/view/view.component';
+import { HomeComponent } from './components/admin/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    AddComponent,
+    DeleteComponent,
+    UpdateComponent,
+    ViewComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
