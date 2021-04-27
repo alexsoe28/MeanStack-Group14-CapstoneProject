1daad0ecfd66a66a1b434734df0d1e0e9d45c571
Austin C <austin.chau@tcs.com>
2021-04-27 10:08:42 -0700

    working routing outlet for admin home

--- FrontEnd/Online-Groceries-frontend/src/app/app-routing.module.ts
+++ FrontEnd/Online-Groceries-frontend/src/app/app-routing.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/admin/home/home.component';
 import { SignInComponent } from './components/admin/signin/signin.component';
+import { AddComponent as AdminAddProductComponent } from "./components/admin/products/add/add.component";
+import { UpdateComponent as AdminUpdateProductComponent } from "./components/admin/products/update/update.component";
+import { DeleteComponent as AdminDeleteProductComponent } from "./components/admin/products/delete/delete.component";
 
 const routes: Routes = [
 	{ path: "admin/signin", component: SignInComponent },
-	{ path: "admin/home", component: HomeComponent },
+	{
+		path: "admin/home", component: HomeComponent,
+		children: [
+			{ path: "addProducts", component: AdminAddProductComponent, outlet: "adminHome" },
+			{ path: "updateProducts", component: AdminUpdateProductComponent, outlet: "adminHome" },
+			{ path: "deleteProducts", component: AdminDeleteProductComponent, outlet: "adminHome" },
+		]
+	},
 	{ path: "", redirectTo: "/admin/signin", pathMatch: "full" },
 ];
 
