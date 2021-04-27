9948c88046ad503dbff55cc5a99d01ff190bf0c2
Austin C <austin.chau@tcs.com>
2021-04-27 10:35:29 -0700

    working admin addProduct Page

--- FrontEnd/Online-Groceries-frontend/src/app/components/admin/products/add/add.component.ts
+++ FrontEnd/Online-Groceries-frontend/src/app/components/admin/products/add/add.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Product } from 'src/app/model/product/product';
 
 @Component({
-  selector: 'app-add',
-  templateUrl: './add.component.html',
-  styleUrls: ['./add.component.css']
+	selector: 'app-add',
+	templateUrl: './add.component.html',
+	styleUrls: ['./add.component.css']
 })
 export class AddComponent implements OnInit {
 
-  constructor() { }
+	addProductForm = this.fb.group({
+		name: ['', [Validators.required]],
+		price: ['', [Validators.required]],
+		quantity: ['', [Validators.required]],
+	})
 
-  ngOnInit(): void {
-  }
+	constructor(private fb: FormBuilder) { }
+
+	ngOnInit(): void { }
+
+	onSubmit() {
+		const [name, price, quantity] = ["name", "price", "quantity"].map(
+			key => this.addProductForm.get(key) as FormControl
+		)
+
+		const invalidCtrl = [name, price, quantity].find(ctrl => ctrl.invalid);
+
+		if (invalidCtrl !== undefined) {
+			console.debug("invalid ctrl: ", invalidCtrl);
+		} else {
+			const product: Product = {
+				name: name.value,
+				price: price.value,
+				quantity: quantity.value,
+			}
+			console.table(product);
+		}
+	}
 
 }
