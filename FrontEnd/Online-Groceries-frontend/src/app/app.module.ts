4959287691f262ababf39eaac6a78bae0b2c94ab
Alex <alexndersoe28@gmail.com>
2021-04-27 22:50:13 -0700

    Can now store cart in an array

--- FrontEnd/Online-Groceries-frontend/src/app/app.module.ts
+++ FrontEnd/Online-Groceries-frontend/src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShoppingPageComponent } from './shopping-page/shopping-page.component';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ShoppingPageComponent
+    ShoppingPageComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
