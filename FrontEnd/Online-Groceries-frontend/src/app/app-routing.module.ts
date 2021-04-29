bbe3d63024b9d53e8a6c661c703afb4d61b69943
Alex <alexndersoe28@gmail.com>
2021-04-28 23:57:46 -0700

    Added cart page

--- FrontEnd/Online-Groceries-frontend/src/app/app-routing.module.ts
+++ FrontEnd/Online-Groceries-frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { AddComponent as AdminAddProductComponent } from "./components/admin/pro
 import { UpdateComponent as AdminUpdateProductComponent } from "./components/admin/products/update/update.component";
 import { DeleteComponent as AdminDeleteProductComponent } from "./components/admin/products/delete/delete.component";
 import { ShoppingPageComponent } from './shopping-page/shopping-page.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { RootComponent } from './components/root/root.component';
 
 const routes: Routes = [
@@ -23,6 +24,7 @@ const routes: Routes = [
 	},
 	// User
 	{ path: "user/browseShop", component: ShoppingPageComponent },
+	{ path: "user/browseShop/myCart", component: ShoppingCartComponent },
 	{ path: "user", redirectTo: "/user/browseShop", pathMatch: "full" },
 	{ path: "admin", redirectTo: "/admin/signin", pathMatch: "full" },
 ];
--- FrontEnd/Online-Groceries-frontend/src/app/app.module.ts
+++ FrontEnd/Online-Groceries-frontend/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ShoppingPageComponent } from './shopping-page/shopping-page.component';
-
 import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
 
+import { ShoppingPageComponent } from './shopping-page/shopping-page.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { SignInComponent } from './components/admin/signin/signin.component';
 import { AddComponent } from './components/admin/products/add/add.component';
 import { DeleteComponent } from './components/admin/products/delete/delete.component';
@@ -14,9 +19,8 @@ import { UpdateComponent } from './components/admin/products/update/update.compo
 import { ViewComponent } from './components/admin/requests/view/view.component';
 import { HomeComponent } from './components/admin/home/home.component';
 import { RootComponent } from './components/root/root.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -39,7 +43,10 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
       AppRoutingModule,
       ReactiveFormsModule,
       FormsModule,
-      HttpClientModule
+      HttpClientModule,
+      MatInputModule,
+      MatFormFieldModule,
+      MatTableModule
     ],
     providers: [],
     bootstrap: [AppComponent]
