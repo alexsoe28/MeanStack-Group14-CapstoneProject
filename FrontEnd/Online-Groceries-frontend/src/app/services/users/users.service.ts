03e91875bf993e7271a9f2e8604c3374895080a6
Austin C <austin.chau@tcs.com>
2021-04-29 16:03:17 -0700

    working admin login

---
+++ FrontEnd/Online-Groceries-frontend/src/app/services/users/users.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { of, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+export type UserRole = "admin" | "customer" | "employee";
+
+@Injectable({
+	providedIn: 'root'
+})
+export class UsersService {
+
+	host = "http://localhost:9090";
+	endpoint = "/users";
+
+	constructor(private http: HttpClient) { }
+
+	signIn(credentials: { username: String, password: String, role: UserRole }) {
+		const url = this.host + this.endpoint + "/login";
+		const payload = {
+			username: credentials.username,
+			password: credentials.password,
+			accessingRole: credentials.role,
+		}
+		return this.http.post<{ userId: String }>(url, payload)
+			.pipe(
+				tap(data => console.table(data)),
+				catchError(error => {
+					if (error instanceof HttpErrorResponse) {
+						if (error.status === 401 || error.status === 403) {
+							return of({ userId: undefined });
+						}
+					}
+					return throwError(error)
+				})
+			);
+	}
+}
