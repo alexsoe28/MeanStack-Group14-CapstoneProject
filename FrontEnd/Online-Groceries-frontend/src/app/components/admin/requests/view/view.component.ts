1697ad5b9be567ffb906f3bd481e1cff6d713899
Austin C <austin.chau@tcs.com>
2021-04-29 09:32:11 -0700

    working admin ticket viewing

--- FrontEnd/Online-Groceries-frontend/src/app/components/admin/requests/view/view.component.ts
+++ FrontEnd/Online-Groceries-frontend/src/app/components/admin/requests/view/view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ticket, TicketsService } from 'src/app/services/tickets/tickets.service';
 
 @Component({
   selector: 'app-view',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewComponent implements OnInit {
 
-  constructor() { }
+	tickets: Ticket[] = [];
+
+  constructor(private ticketsService: TicketsService) { }
 
   ngOnInit(): void {
-  }
+		this.ticketsService.getAdminRequests()
+			.subscribe(data => this.tickets = data);
+	}
 
 }
