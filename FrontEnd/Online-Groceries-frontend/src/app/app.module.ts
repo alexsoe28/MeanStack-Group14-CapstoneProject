19f6f58d3c5163e1a298c4b1a06d4acd4aeb91a3
Gibran Khalil <gkhalil98@gmail.com>
2021-04-29 00:33:35 -0400

    pushed auxiliary files ex. package-lock, app.module.ts etc

--- FrontEnd/Online-Groceries-frontend/src/app/app.module.ts
+++ FrontEnd/Online-Groceries-frontend/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShoppingPageComponent } from './shopping-page/shopping-page.component';
+import { EmployeeRequestComponent } from './employee-request/employee-request.component';
+import { UpdateStatusComponent } from './update-status/update-status.component';
+import { UnlockUserComponent } from './unlock-user/unlock-user.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShoppingPageComponent
+    ShoppingPageComponent,
+    EmployeeRequestComponent,
+    UpdateStatusComponent,
+    UnlockUserComponent,
+    EditProfileComponent
   ],
   imports: [
     BrowserModule,
