f50e83720dca4fccb4fcd6507250d035a7d0364a
Austin C <austin.chau@tcs.com>
2021-04-30 03:31:25 -0700

    styling admin request page

--- FrontEnd/Online-Groceries-frontend/src/app/components/admin/requests/view/view.component.ts
+++ FrontEnd/Online-Groceries-frontend/src/app/components/admin/requests/view/view.component.ts
@@ -2,17 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { Ticket, TicketsService } from 'src/app/services/tickets/tickets.service';
 
 @Component({
-  selector: 'app-view',
-  templateUrl: './view.component.html',
-  styleUrls: ['./view.component.css']
+	selector: 'app-view',
+	templateUrl: './view.component.html',
+	styleUrls: ['./view.component.css']
 })
 export class ViewComponent implements OnInit {
 
 	tickets: Ticket[] = [];
+	ticketColumn = ["ticketId", "userId", "message"];
 
-  constructor(private ticketsService: TicketsService) { }
+	constructor(private ticketsService: TicketsService) { }
 
-  ngOnInit(): void {
+	ngOnInit(): void {
 		this.ticketsService.getAdminRequests()
 			.subscribe(data => this.tickets = data);
 	}
